c72cacc6489b6d78a02e0085e261733a3f4be0e8
debbie <drosenfeld87@gmail.com>
2018-03-14 18:07:34 -0400

    3 methods working now

---
+++ simon_says.rb
@@ -0,0 +1,18 @@
+def echo(string)
+  hello
+end
+
+
+def shout(string)
+  string.upcase
+end
+
+def shout_multiple_words(string)
+end
+
+def repeat(string, n=3)
+  ( "#{string} " * n ).strip
+end
+
+def start_of_word(string)
+end
